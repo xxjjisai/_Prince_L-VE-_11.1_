da21f374c051137be4317ff36cb5585f0bbef08f
xxjjisai <1353231305@qq.com>
2019-01-09 13:56:08 +0800

    first add

--- run.bat
+++ run.bat
@@ -1,11 +1,11 @@
 "D:\Software\LOVE2D\LOVE11.1\LOVE\love.exe" "%cd%"
 
-REM ::-- init
-REM ::git init
-REM ::git remote add origin  https://github.com/xxjjisai/_Prince_L-VE-_11.1_.git
-REM ::git pull origin master
+::-- init
+::git init
+::git remote add origin  https://github.com/xxjjisai/_Prince_L-VE-_11.1_.git
+::git pull origin master
 
-REM ::-- add
-REM ::git add main.lua
-REM ::git commit -m 'log'
-REM ::git push origin master
+::-- add
+::git add main.lua
+::git commit -m 'log'
+::git push origin master
