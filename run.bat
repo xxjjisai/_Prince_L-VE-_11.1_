e1e8fa0cb11280075324fa1f054cc1cd0ba7f171
xxjjisai <1353231305@qq.com>
2019-01-09 13:35:06 +0800

    first add

--- run.bat
+++ run.bat
@@ -1,1 +1,11 @@
 "D:\Software\LOVE2D\LOVE11.1\LOVE\love.exe" "%cd%"
+
+-- init
+git init
+git remote add origin  https://github.com/xxjjisai/_Prince_L-VE-_11.1_.git
+git pull origin master
+
+-- add
+git add main.lua
+git commit -m 'log'
+git push origin master
